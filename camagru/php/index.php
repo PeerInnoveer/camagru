a77f0e38fc47079ac7587724dec96efabd3d876c
Pierre van Rensburg <peerinnoveer@gmail.com>
2019-01-11 15:38:04 +0200

    Add files via upload
    a few more things to complete.

---
+++ camagru/php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<?php
+    require 'header.php';
+?>
+
+<!-- Main content -->
+<div class="main-content">
+    <!-- gallery of photos -->
+    <div class="gallery">
+
+    </div>
+
+</div>
+
+<?php
+    require 'footer.php';
+?>
+
+</body>
