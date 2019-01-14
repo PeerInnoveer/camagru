7f3a1f05b148bb3c6c1562d50d8e45b5d90fa4d8
Pierre VAN RENSBURG <pvan-ren@c2r1s1.wethinkcode.co.za>
2019-01-14 08:42:18 +0200

    Delete camagru directory

--- camagru/includes/logout.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (isset($_POST['logout-submit'])) {
-    session_start();
-    session_unset();
-    session_destroy();
-    header("Location: ../php/signup.php?logout=success");
-    exit();
-}
