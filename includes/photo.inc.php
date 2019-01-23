2bf4c672e395f43a7a8dc2c741299490814cccbe
Pierre VAN RENSBURG <pvan-ren@c1r1s2.wethinkcode.co.za>
2019-01-23 15:50:38 +0200

    Update

--- includes/photo.inc.php
+++ includes/photo.inc.php
@@ -2,8 +2,11 @@
 
 require 'dbh.inc.php';
 
-if (isset($_POST['user_uid'])) {
+if ((isset($_POST['user_uid'])) && (isset($_POST['picture']))) {
     
+    $username = $_POST['user_uid'];
     $picture = $_POST['picture'];
+    
+    //To do: bind paramaters, prepare statement and execute.
     $resultSet = $conn->query("INSERT INTO images WHERE 'image'");
 }
