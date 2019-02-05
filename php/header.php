7ebfd44ba125be6c663031ae3af023bbd1a479b4
Pierre VAN RENSBURG <pvan-ren@c2r2s3.wethinkcode.co.za>
2019-02-05 12:43:27 +0200

    Update

--- php/header.php
+++ php/header.php
@@ -16,47 +16,43 @@
 </head>
 <body>
 <header>
-    <nav>
-        <div class="navbar">
-            <?php
-                if (isset($_SESSION['userUid'])) {
-                    echo '<div class="navbar-logo">
-                    <a href="index.php"><strong>CamGuru</strong></a>
-                    <a href="signup.php"><i class="camlogo fas fa-camera"></i></a>
+    <nav class="navbar">
+        <?php
+            if (isset($_SESSION['userUid'])) {
+                echo '<div class="navbar-logo">
+                        <a href="index.php"><strong>CamGuru</strong></a>
+                        <a href="signup.php"><i class="camlogo fas fa-camera"></i></a>
                     </div>';
                 } else echo '<div class="navbar-logo">
-                <a href="signup.php"><strong>CamGuru</strong></a>
-                <a href="signup.php"><i class="camlogo fas fa-camera"></i></a>
-                </div>';
-            ?>
-            <?php
-                    if (isset($_SESSION['userUid'])) {
-                        echo '<ul> <!-- floats to right -->
-                                <li><a class="username_style" href="profile.php">'.$_SESSION["userUid"].'</a></li>
-                                <li><a href="photo_index.php"><i class="cam_icon fa fa-camera-retro"></i></a></li>
-                                <li><a href="#"><i class="settings_icon fas fa-cog"></i></a>
-                                    <ul>
-                                        <!-- <li><a href="#"</a>Update Account</li> -->
-                                        <li>
-                                            <form action="../includes/logout.inc.php" method="POST">
-                                                <button class="btn" type="submit" name="logout-submit">Logout</button>
-                                            </form>
-                                        </li>
-                                    </ul>
+                        <a href="signup.php"><strong>CamGuru</strong></a>
+                        <a href="signup.php"><i class="camlogo fas fa-camera"></i></a>
+                    </div>';
+        ?>
+        <?php
+            if (isset($_SESSION['userUid'])) {
+                echo '<ul> <!-- floats to right -->
+                        <li><a class="username_style" href="profile.php">'.$_SESSION["userUid"].'</a></li>
+                        <li><a href="photo_index.php"><i class="cam_icon fa fa-camera-retro"></i></a></li>
+                        <li><a href="#"><i class="settings_icon fas fa-cog"></i></a>
+                            <ul>
+                                <li><a href="#">Update Account</a></li>
+                                <li><a href="#">Notifications</a></li>
+                                <li><a href="#">Send App Feedback</a></li>
+                                    <form action="../includes/logout.inc.php" method="POST">
+                                        <button class="btn" type="submit" name="logout-submit">Logout</button>
+                                    </form>
                                 </li>
-                            </ul';
-                        } else {
-                            echo '<form class="login-form" action="../includes/login.inc.php" method="POST">
-                            <input type="text" name="username" placeholder="Username">
-                            <input type="password" name="pwd" placeholder="Password">
-                            <button type="submit" name="login-submit">Login</button>
-                            </form>';
-                            //header("Location: ../includes/signup.inc.php");
-                        }
-                    ?>
-                <?php
-                ?>
-            </ul> 
-        </div>
+                            </ul>
+                        </li>
+                    </ul';
+                    } else {
+                        echo '<form class="login-form" action="../includes/login.inc.php" method="POST">
+                        <input type="text" name="username" placeholder="Username">
+                        <input type="password" name="pwd" placeholder="Password">
+                        <button class="login" type="submit" name="login-submit">Login</button>
+                        </form>';
+                        //header("Location: ../includes/signup.inc.php");
+                    }
+        ?>
     </nav>
 </header>
