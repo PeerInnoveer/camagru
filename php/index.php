8efec84676f210a241e480e7acb1ed9eb0a22291
Pierre VAN RENSBURG <pvan-ren@c2r6s6.wethinkcode.co.za>
2019-02-11 12:33:17 +0200

    Update

--- php/index.php
+++ php/index.php
@@ -19,10 +19,13 @@
                 echo '<div class="image_container"><img class="images" src="'.$image[0].'"/>
                         <div class="button_container">
                             <form action="../includes/del_like_com.inc.php" method="POST">
-                                <button class="delBut" type="submit" name="photoDel"><i class="delB fas fa-trash-alt"></i></button>
-                                <button class="likeBut" type="submit" name="like_button"><i class=" likeB fas fa-thumbs-up"></i></button>
-                                <button class="commentBut" type="submit" name="com_button"><i class=" commentB fas fa-comments"></i></button>
-                            </form>
+                                <button class="delBut" type="submit" name="photoDel"><i class=" delB far fa-trash-alt"></i></button>
+                                <button class="likeBut" type="submit" name="like_button"><i class=" likeB far fa-heart"></i></button>
+                                <button class="commentBut" type="submit" name="com_button"><i class=" commentB far fa-comment"></i></button>
+                                </form>
+                                <section class="cmsc">
+                                    <textarea aria-label="Add a comment" class="tacn" name="add_com" id="comment" placeholder="Add comment..."></textarea>
+                                </section>
                         </div>
                     </div>';
         } catch(PDOException $e) {
